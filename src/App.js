4fc47786704ccc652c91f260cff77b21322f47c7
mrmisiex <misiek10061998@gmail.com>
2021-09-16 20:47:26 +0200

    Connected with api, added inputs,. styling and validation to do

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Button, Container, Input, InputAdornment } from "@mui/material";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 
-function App() {
+const App = () => {
+  const axios = require("axios");
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const API = `/api/email-validator.php?email=${email}`;
+
+  const validation = (name) => {
+    if (name > 2) {
+    }
+  };
+
+  const handleValid = async () => {
+    try {
+      const response = await axios.get(API);
+
+      console.log(response.data.status_message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xl">
+      <Input
+        id="input-with-icon-adornment"
+        label="name"
+        variant="outlined"
+        onChange={(e) => setName(e.target.value)}
+        endAdornment={
+          <InputAdornment position="end">
+            <ErrorOutlineIcon />
+          </InputAdornment>
+        }
+      />
+      <Input
+        id="input-with-icon-adornment"
+        label="email"
+        variant="outlined"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Button variant="text" onClick={() => handleValid()}>
+        Submit!
+      </Button>
+    </Container>
   );
-}
+};
 
 export default App;
